61552193fbb44a96e9a225de6d859d5138cd313d
Vlad Adaskalitsa <Moonspellsablle@MacBook-Air-Vlad.local>
2019-07-28 23:50:38 +0300

    Update#3: Removed cons global variables. Added auto type for DISCRIMINANT const

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cmath>
 
-const int RANGE_UPPER_VALUE = 600;
-const int RANGE_LOWER_VALUE = -600;
-
 int getParameter(char parameter) {
   std::cout << "Input coefficient in range [-600..600] <" << parameter << ">: ";
   int coeff_a;
+  const int RANGE_UPPER_VALUE = 600;
+  const int RANGE_LOWER_VALUE = -600;
   while (!(std::cin >> coeff_a) || (coeff_a < RANGE_LOWER_VALUE || coeff_a > RANGE_UPPER_VALUE)) {
     std::cout << "Error! Re-Input coefficient <" << parameter << ">" << std::endl;
     std::cin.clear();
@@ -15,9 +14,8 @@ int getParameter(char parameter) {
   return coeff_a;
 }
 
-long long func_discr(int a, int b, int c) {
-  const long long DISCRIMINANT = b * b - 4 * a * c;
-  return DISCRIMINANT;
+auto func_discr(int a, int b, int c) {
+  return b * b - 4 * a * c;
 }
 
 int main() {
@@ -27,7 +25,7 @@ int main() {
   const int SEC_KOEF = getParameter('b');
   const int THIRD_KOEF = getParameter('c');
 
-  const long long DISCRIMINANT = func_discr(FIRST_KOEF, SEC_KOEF, THIRD_KOEF);
+  const auto DISCRIMINANT = func_discr(FIRST_KOEF, SEC_KOEF, THIRD_KOEF);
   if (DISCRIMINANT > 0) {
     const int DOMINATOR = 2 * FIRST_KOEF;
     const double RESULT_1 = (-SEC_KOEF + sqrt(DISCRIMINANT)) / DOMINATOR;
