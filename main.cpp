9b9b40317dde4c092a4a86da29c99a1ab46df002
Vlad Adaskalitsa <Moonspellsablle@MacBook-Air-Vlad.local>
2019-07-28 23:13:32 +0300

    Update#2 :) Implemented all changes, from comments.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
 
-const int rangeUpperValue = 600;
-const int rangeLowerValue = -600;
+const int RANGE_UPPER_VALUE = 600;
+const int RANGE_LOWER_VALUE = -600;
 
-double getParameter(char parameter) {
+int getParameter(char parameter) {
   std::cout << "Input coefficient in range [-600..600] <" << parameter << ">: ";
   int coeff_a;
-  while (!(std::cin >> coeff_a) || !(coeff_a >= rangeLowerValue && coeff_a <= rangeUpperValue)) {
+  while (!(std::cin >> coeff_a) || (coeff_a < RANGE_LOWER_VALUE || coeff_a > RANGE_UPPER_VALUE)) {
     std::cout << "Error! Re-Input coefficient <" << parameter << ">" << std::endl;
     std::cin.clear();
     std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
@@ -15,29 +15,33 @@ double getParameter(char parameter) {
   return coeff_a;
 }
 
-int func_discr(int a, int b, int c) {
-  const int discriminant = b * b - 4 * a * c;
-  return discriminant;
+long long func_discr(int a, int b, int c) {
+  const long long DISCRIMINANT = b * b - 4 * a * c;
+  return DISCRIMINANT;
 }
 
 int main() {
   std::cout << "The quadratic equation has the following form: ax^2 + bx + c" << std::endl << std::endl;
 
-  const int first_koef = getParameter('a');
-  const int sec_koef = getParameter('b');
-  const int third_koef = getParameter('c');
+  const int FIRST_KOEF = getParameter('a');
+  const int SEC_KOEF = getParameter('b');
+  const int THIRD_KOEF = getParameter('c');
 
-  const int discriminant = func_discr(first_koef, sec_koef, third_koef);
-  if (discriminant > 0) {
-    const int dominator = 2 * first_koef;
-    const int result_1 = (-sec_koef + sqrt(discriminant)) / dominator;
-    const int result_2 = (-sec_koef - sqrt(discriminant)) / dominator;
+  const long long DISCRIMINANT = func_discr(FIRST_KOEF, SEC_KOEF, THIRD_KOEF);
+  if (DISCRIMINANT > 0) {
+    const int DOMINATOR = 2 * FIRST_KOEF;
+    const double RESULT_1 = (-SEC_KOEF + sqrt(DISCRIMINANT)) / DOMINATOR;
+    const double RESULT_2 = (-SEC_KOEF - sqrt(DISCRIMINANT)) / DOMINATOR;
 
-    result_1 == result_2 ? std::cout << "X = " << result_1 << std::endl : std::cout << "X1 = " << result_1 << "\tX2 = " << result_2 << std::endl;
+    if (RESULT_1 == RESULT_2) {
+      std::cout << "X = " << RESULT_1 << std::endl;
+    } else {
+      std::cout << "X1 = " << RESULT_1 << "\tX2 = " << RESULT_2 << std::endl;
+    }
   } else {
     std::cout << "CRITICAL ERROR! D < 0!" << std::endl;
   }
-  
+
   return 0;   
 }
 
