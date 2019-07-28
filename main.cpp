eff4b0145e0ada9a000d9bb3c6d5a7f008492d5e
Vlad Adaskalitsa <Moonspellsablle@MacBook-Air-Vlad.local>
2019-07-28 22:42:14 +0300

    Implemented all changes, from comments. Thanks.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,41 @@
 #include <iostream>
 #include <cmath>
 
-//Prototype
-double getParameter(char parameter);
-double func_discr(double a, double b, double c, double discriminant);
+const int rangeUpperValue = 600;
+const int rangeLowerValue = -600;
 
 double getParameter(char parameter) {
-  do {
-    std::cout << "Please enter '" << parameter << "' in range[-600..600]: " << std::endl;
-    double koef;
-    if(std::cin >> koef) {
-      if (koef >= -600 && koef <= 600) {
-        return koef;
-      }
-    } else {
-      std::cout << "Only digit is avalible for parameter value!!!";
-      throw 1;
-    }
+  std::cout << "Input coefficient in range [-600..600] <" << parameter << ">: ";
+  int coeff_a;
+  while (!(std::cin >> coeff_a) || !(coeff_a >= rangeLowerValue && coeff_a <= rangeUpperValue)) {
+    std::cout << "Error! Re-Input coefficient <" << parameter << ">" << std::endl;
+    std::cin.clear();
+    std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
   }
-  while(true);
+  return coeff_a;
 }
 
-double func_discr(double a, double b, double c, double discriminant) {
-  discriminant = b * b - 4 * a * c;
-  if(discriminant < 0) {
-    std::cout << "CRITICAL ERROR! D < 0!" << std::endl;
-    return discriminant;
-  } else {         
-    return discriminant;
-  }
+int func_discr(int a, int b, int c) {
+  const int discriminant = b * b - 4 * a * c;
+  return discriminant;
 }
 
 int main() {
-  double first_koef = 0, sec_koef = 0, third_koef = 0;
-  double discriminant = 0;
-  double result_1 = 0, result_2 = 0;
-
   std::cout << "The quadratic equation has the following form: ax^2 + bx + c" << std::endl << std::endl;
 
-  first_koef = getParameter('a');
-  sec_koef = getParameter('b');
-  third_koef = getParameter('c');
+  const int first_koef = getParameter('a');
+  const int sec_koef = getParameter('b');
+  const int third_koef = getParameter('c');
 
-  discriminant = func_discr(first_koef, sec_koef, third_koef, discriminant);
+  const int discriminant = func_discr(first_koef, sec_koef, third_koef);
   if (discriminant > 0) {
-    result_1 = (-sec_koef + sqrt(discriminant)) / 2 * first_koef;
-    result_2 = (-sec_koef - sqrt(discriminant)) / 2 * first_koef;
+    const int dominator = 2 * first_koef;
+    const int result_1 = (-sec_koef + sqrt(discriminant)) / dominator;
+    const int result_2 = (-sec_koef - sqrt(discriminant)) / dominator;
 
-    if (result_1 == result_2) {
-    	std::cout << "X = " << result_1 << std::endl;
-    } else {
-    	std::cout << "X1 = " << result_1 << "\tX2 = " << result_2 << std::endl;
-    }
+    result_1 == result_2 ? std::cout << "X = " << result_1 << std::endl : std::cout << "X1 = " << result_1 << "\tX2 = " << result_2 << std::endl;
+  } else {
+    std::cout << "CRITICAL ERROR! D < 0!" << std::endl;
   }
   
   return 0;   
